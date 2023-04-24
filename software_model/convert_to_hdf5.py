63393995805efe70dcf5522e898be43ff31d3aaf
Georg Wiese <georgwiese@gmail.com>
2023-04-24 15:31:23 +0200

    Implement HDF5 export

---
+++ software_model/convert_to_hdf5.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import lzma
+import pickle
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+
+def convert_model(model_fname):
+    print("Loading model")
+    with lzma.open(model_fname, "rb") as f:
+        state_dict = pickle.load(f)
+
+    model = state_dict["model"]
+    info = state_dict["info"]
+
+    assert model_fname.endswith(".lzma")
+    output_path = Path(model_fname[:-5] + ".hdf5")
+
+    if output_path.exists():
+        print("Deleting existing output file")
+        output_path.unlink()
+
+    with h5py.File(str(output_path), "w") as f:
+        bloom_filters = np.array([
+            [
+                bloom_filter.data
+                for bloom_filter in discriminator.filters
+            ]
+            for discriminator in model.discriminators
+        ])
+
+        assert bloom_filters.min() == 0
+        assert bloom_filters.max() == 1
+        bloom_filters = bloom_filters.astype(bool)
+
+        expected_shape = (info["num_classes"], int(info["num_inputs"] * info["bits_per_input"] / info["num_filter_inputs"]), info["num_filter_entries"])
+        assert bloom_filters.shape == expected_shape
+
+        f.create_dataset("bloom_filters", data=bloom_filters)
+        for k, v in info.items():
+            f.attrs[k] = v
+
+def read_arguments():
+    parser = argparse.ArgumentParser(description="Convert a given model to HDF5")
+    parser.add_argument("model_fname", help="Path to pretrained model .pickle.lzma")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = read_arguments()
+    convert_model(args.model_fname)
+
